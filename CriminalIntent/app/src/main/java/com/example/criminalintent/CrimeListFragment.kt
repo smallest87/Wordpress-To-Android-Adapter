7b1e8609e6bffeba36b6f09138a59909a2c01d40
Mike <72737016+DarkiZZZ@users.noreply.github.com>
2022-04-22 18:11:44 +0300

    started to make RecyclerView for Fragment

---
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,48 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.criminalintent.databinding.FragmentCrimeListBinding
+
+private const val TAG = "CrimeListFragment"
+class CrimeListFragment: Fragment() {
+
+    private lateinit var binding: FragmentCrimeListBinding
+
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProvider(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total Crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeListBinding.inflate(inflater, container, false)
+        binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return binding.root
+    }
+
+    companion object{
+        fun newInstance(): CrimeListFragment{
+            return CrimeListFragment()
+        }
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view){
+        
+    }
+}
--- CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -7,12 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crime)
+        supportActionBar?.hide()
 
         val currentFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         if (currentFragment == null){
-            val fragment = CrimeFragment()
+            val fragment = CrimeListFragment.newInstance()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
