f72ae24065aaf2ae14719d2f7c9d914373a1ed20
Michail Sokolov <mark_sokolov_2016@mail.ru>
2022-04-28 15:29:09 +0300

    Added Holder and Adapter to RecyclerView. Made it working

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -31,7 +32,7 @@ class CrimeListFragment: Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentCrimeListBinding.inflate(inflater, container, false)
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
         updateUI()
@@ -44,9 +45,27 @@ class CrimeListFragment: Fragment() {
         }
     }
 
-    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view){
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener{
+
+        private lateinit var crime: Crime
+
         val titleTextView: TextView = itemView.findViewById(R.id.crimeTitle)
         val dateTextView: TextView = itemView.findViewById(R.id.crimeDate)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+        
+        fun bind(crime:Crime){
+            this.crime = crime
+            titleTextView.text = crime.title
+            dateTextView.text = crime.date.toString()
+        }
+
+        override fun onClick(p0: View?) {
+            Toast.makeText(context, "${crime.title} pressed", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     private inner class CrimeAdapter(var crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>(){
@@ -61,10 +80,7 @@ class CrimeListFragment: Fragment() {
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title
-                dateTextView.text = crime.date.toString()
-            }
+            holder.bind(crime)
         }
 
         override fun getItemCount() = crimes.size
