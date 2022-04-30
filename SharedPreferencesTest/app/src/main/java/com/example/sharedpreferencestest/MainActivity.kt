3ec22a79a339a3d8ddcb6c7226b84400e8fc6581
Michail Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-01 01:28:27 +0300

    made XML for text

--- SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.kt
+++ SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.sharedpreferencestest
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sharedpreferencestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var preferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
