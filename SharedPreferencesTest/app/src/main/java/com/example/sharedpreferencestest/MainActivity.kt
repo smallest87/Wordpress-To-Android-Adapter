be742973314dafa7d99f0c1a6cdb0f6f5d0d3889
Michail Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-01 01:59:09 +0300

    registered SharedPreferences listener

--- SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.kt
+++ SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.kt
@@ -1,19 +1,49 @@
 package com.example.sharedpreferencestest
 
+import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sharedpreferencestest.databinding.ActivityMainBinding
 
+const val APP_PREFERENCES = " app preferences"
+const val PREFERENCES_TEXT_KEY_VALUE = "key value"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
     private lateinit var preferences: SharedPreferences
 
+    private val preferencesListener = SharedPreferences.OnSharedPreferenceChangeListener{preferences, key ->
+        if (key == PREFERENCES_TEXT_KEY_VALUE){
+            binding.savedSharedPreferencesTextView.text = preferences.getString(key, "")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        preferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+        val currentValue = preferences.getString(PREFERENCES_TEXT_KEY_VALUE, "")
+        binding.editTextSharedPreferences.setText(currentValue)
+        binding.savedSharedPreferencesTextView.text = currentValue
 
+        binding.saveFromEditTextButton.setOnClickListener {
+            val value = binding.editTextSharedPreferences.text.toString()
+            preferences.edit()
+                .putString(PREFERENCES_TEXT_KEY_VALUE, value)
+                .apply()
+        }
+        binding.clearButton.setOnClickListener {
+            binding.editTextSharedPreferences.setText("")
+        }
+
+        preferences.registerOnSharedPreferenceChangeListener(preferencesListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        preferences.unregisterOnSharedPreferenceChangeListener(preferencesListener)
     }
 }
