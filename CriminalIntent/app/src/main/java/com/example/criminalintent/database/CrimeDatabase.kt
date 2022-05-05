8c092d9b6f33a14107fdfc406c3bce324c515c57
Mike <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-05 16:04:53 +0300

    migrate DB to v.2

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -1,10 +1,12 @@
 package com.example.criminalintent.database
 
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.example.criminalintent.Crime
 
-@Database(entities = [Crime::class], version = 1)
+@Database(entities = [Crime::class], version = 2)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase: RoomDatabase() {
     override fun createOpenHelper(config: DatabaseConfiguration?): SupportSQLiteOpenHelper {
@@ -20,4 +22,12 @@ abstract class CrimeDatabase: RoomDatabase() {
     }
 
     abstract fun crimeDao(): CrimeDAO
+}
+
+val migration_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE Crime ADD COLUMN suspect TEXT NOT NULL DEFAULT ''"
+        )
+    }
 }
