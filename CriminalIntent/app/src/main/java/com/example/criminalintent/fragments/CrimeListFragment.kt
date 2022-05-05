24ab228e58bf8cae56effd949b4a24a6963626a7
Mike <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-05 13:49:13 +0300

    made adding crimes work

--- CriminalIntent/app/src/main/java/com/example/criminalintent/fragments/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/fragments/CrimeListFragment.kt
@@ -3,9 +3,7 @@ package com.example.criminalintent.fragments
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -36,6 +34,10 @@ class CrimeListFragment: Fragment() {
         ViewModelProvider(this).get(CrimeListViewModel::class.java)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     override fun onAttach(context: Context) {
         super.onAttach(context)
         callbacks = context as Callbacks?
@@ -152,5 +154,22 @@ class CrimeListFragment: Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.new_crime ->{
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
--- CriminalIntent/app/src/main/java/com/example/criminalintent/viewmodels/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/viewmodels/CrimeListViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.criminalintent.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.criminalintent.Crime
 import com.example.criminalintent.database.CrimeRepository
 
 class CrimeListViewModel: ViewModel() {
 
     private val crimeRepository = CrimeRepository.get()
     val crimesLiveData = crimeRepository.getListOfCrimesRepo()
+
+    fun addCrime(crime: Crime){
+        crimeRepository.addCrime(crime)
+    }
 }
