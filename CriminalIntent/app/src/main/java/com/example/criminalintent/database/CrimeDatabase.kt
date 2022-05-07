95af29227632a8bd8d6b4a19cffb58ebcd923677
Michail Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-07 20:00:43 +0300

    added migration v.3 with phone number

--- CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -31,3 +31,12 @@ val migration_1_2 = object : Migration(1, 2) {
         )
     }
 }
+
+val migration_1_3 = object : Migration(2, 3){
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE Crime ADD COLUMN phone_number TEXT NOT NULL DEFAULT ''"
+        )
+    }
+
+}
