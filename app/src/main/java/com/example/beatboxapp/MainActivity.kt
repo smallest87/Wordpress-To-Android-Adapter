3208432e472ca545aecfc1420321010278ebf993
Mike <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-10 14:33:42 +0300

    added dataBinding to main activity

--- app/src/main/java/com/example/beatboxapp/MainActivity.kt
+++ app/src/main/java/com/example/beatboxapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.beatboxapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.beatboxapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
