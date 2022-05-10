6e24b38c38fd1a527254106bfc2ad1095b0ec00f
Mike <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-10 15:31:48 +0300

    added class for sound

---
+++ app/src/main/java/com/example/beatboxapp/Sound.kt
@@ -0,0 +1,9 @@
+package com.example.beatboxapp
+
+private const val WAV = ".wav"
+
+class Sound(private val assetPath: String) {
+    val name = assetPath
+        .split("/")
+        .last().removeSuffix(WAV)
+}
