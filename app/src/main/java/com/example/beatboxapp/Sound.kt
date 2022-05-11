62a94c74d63129b00745ccb7e8eb94b46a722419
Michail Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-11 16:35:26 +0300

    .

--- app/src/main/java/com/example/beatboxapp/Sound.kt
+++
@@ -1,9 +0,0 @@
-package com.example.beatboxapp
-
-private const val WAV = ".wav"
-
-class Sound(private val assetPath: String) {
-    val name = assetPath
-        .split("/")
-        .last().removeSuffix(WAV)
-}
