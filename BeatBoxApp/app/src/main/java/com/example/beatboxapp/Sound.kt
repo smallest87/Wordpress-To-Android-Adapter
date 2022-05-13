446c7c62bcce4b637128163b0ee9915dcc35e6cd
Michail Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-13 21:05:40 +0300

    loading sounds

--- BeatBoxApp/app/src/main/java/com/example/beatboxapp/Sound.kt
+++ BeatBoxApp/app/src/main/java/com/example/beatboxapp/Sound.kt
@@ -2,6 +2,6 @@ package com.example.beatboxapp
 
 private const val WAV = ".wav"
 
-class Sound(private val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
     val name = assetPath.split("/").last().removeSuffix(WAV)
 }
