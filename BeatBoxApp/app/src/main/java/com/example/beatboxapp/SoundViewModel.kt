f865137afe0a23660a480092a3835b61dc99b864
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-13 16:14:56 +0300

    added binding ViewModel to View

--- BeatBoxApp/app/src/main/java/com/example/beatboxapp/SoundViewModel.kt
+++ BeatBoxApp/app/src/main/java/com/example/beatboxapp/SoundViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.beatboxapp
 
 class SoundViewModel {
+
+    var sound: Sound? = null
+    set(sound) {
+        field =sound
+    }
+
+    val title: String?
+    get() = sound?.name
 }
