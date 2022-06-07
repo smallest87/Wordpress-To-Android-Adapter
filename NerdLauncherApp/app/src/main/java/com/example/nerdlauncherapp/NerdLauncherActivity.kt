b307b311a3d1ecd8e3146e69dccf09e6a90dc4a1
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-06-07 15:15:46 +0300

    refactoring main of app

--- NerdLauncherApp/app/src/main/java/com/example/nerdlauncherapp/NerdLauncherActivity.kt
+++ NerdLauncherApp/app/src/main/java/com/example/nerdlauncherapp/NerdLauncherActivity.kt
@@ -3,9 +3,9 @@ package com.example.nerdlauncherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NerdLauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nerd_launcher)
     }
 }
