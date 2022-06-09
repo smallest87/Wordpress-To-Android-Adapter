97593fd7518b604830a846ff9584edd5ea08bc27
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-06-09 16:41:00 +0300

    init commit

---
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.photogallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_gallery)
+    }
+}
