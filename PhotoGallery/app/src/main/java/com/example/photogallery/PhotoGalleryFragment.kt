0223c0ea5e14456e4494ca755ae65d496c40d46e
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-06-09 20:23:32 +0300

    connected converter

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.photogallery.api.FlickrApi
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class PhotoGalleryFragment : Fragment(){
 
@@ -19,6 +20,7 @@ class PhotoGalleryFragment : Fragment(){
 
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
             .build()
         val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
 
