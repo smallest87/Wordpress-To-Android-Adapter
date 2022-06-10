d4c617feb304b4eb33b5cfc586c9fc9ff94b8b58
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-06-10 17:27:19 +0300

    reorg code to work with LiveData

---
+++ PhotoGallery/app/src/main/java/com/example/photogallery/FlickrFetchr.kt
@@ -0,0 +1,44 @@
+package com.example.photogallery
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "FlickrFetchr"
+
+class FlickrFetchr {
+
+    private val flickrApi: FlickrApi
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    fun fetchContents(): LiveData<String>{
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrRequest.enqueue(object : Callback<String>{
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received")
+                responseLiveData.value = response.body()
+            }
+        })
+        return responseLiveData
+    }
+}
