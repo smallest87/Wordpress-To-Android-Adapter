08437406b822d2a624b325b75d2518ad5e8a1883
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-06-11 19:58:36 +0300

    added class to compare with JSON photo

---
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/PhotoResponse.kt
@@ -0,0 +1,9 @@
+package com.example.photogallery.api
+
+import com.example.photogallery.GalleryItem
+import com.google.gson.annotations.SerializedName
+
+class PhotoResponse {
+    @SerializedName("photo")
+    lateinit var galleryItems: List<GalleryItem>
+}
