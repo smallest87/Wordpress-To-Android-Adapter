bdb0d7327a177e351a8804f0fe374ea5bba6bdab
Julian Sukrisna <smallest87@gmail.com>
2022-06-13 19:27:58 +0700

    Membuat landing page pada TambahanActivity.kt

--- CafeOrderApp/app/src/main/java/com/example/cafeorderapp/TambahanActivity.kt
+++ CafeOrderApp/app/src/main/java/com/example/cafeorderapp/TambahanActivity.kt
@@ -1,4 +1,14 @@
 package com.example.cafeorderapp
 
-class TambahanActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cafeorderapp.databinding.ActivityTambahanBinding
+
+class TambahanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambahan)
+    }
+
 }
