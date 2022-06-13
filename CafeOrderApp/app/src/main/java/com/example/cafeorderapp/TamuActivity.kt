0fbca0945c9c04ac333afdc458a574187ab9a6ed
Julian Sukrisna <smallest87@gmail.com>
2022-06-13 19:47:51 +0700

    menerapkan Binding View pada TamuActivity.kt

--- CafeOrderApp/app/src/main/java/com/example/cafeorderapp/TamuActivity.kt
+++ CafeOrderApp/app/src/main/java/com/example/cafeorderapp/TamuActivity.kt
@@ -2,11 +2,16 @@ package com.example.cafeorderapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cafeorderapp.databinding.ActivityTamuBinding
 
 class TamuActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityTamuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tamu)
+
+        binding = ActivityTamuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
