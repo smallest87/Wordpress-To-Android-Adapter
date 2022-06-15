f9bc45f0f4a89b96f29839bfc3dc54bbe17e6384
Julian Sukrisna <smallest87@gmail.com>
2022-06-15 12:21:50 +0700

    Update RuangTamu.kt

--- CafeOrderApp/app/src/main/java/com/example/cafeorderapp/RuangTamu.kt
+++ CafeOrderApp/app/src/main/java/com/example/cafeorderapp/RuangTamu.kt
@@ -3,9 +3,20 @@ package com.example.cafeorderapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cafeorderapp.databinding.LayarRuangTamuBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RuangTamu : AppCompatActivity() {
+
+//    val POSTS: String = "posts"
+//    val PER_HALAMAN: String = "per_page"
+//    val JUMLAH_HALAMAN: Int = 3
+
+
+    private val list = ArrayList<PostResponse>()
 
     private lateinit var binding: LayarRuangTamuBinding
 
@@ -14,6 +25,25 @@ class RuangTamu : AppCompatActivity() {
 
         binding = LayarRuangTamuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvPost.setHasFixedSize(true)
+        binding.rvPost.layoutManager = LinearLayoutManager(this)
+
+        RetrofitClient.instance.getPosts().enqueue(object: Callback<ArrayList<PostResponse>>{
+            override fun onResponse(
+                call: Call<ArrayList<PostResponse>>,
+                response: Response<ArrayList<PostResponse>>
+            ) {
+                val responseCode = response.code().toString()
+                response.body()?.let { list.addAll(it)}
+                val adapter = PostAdapter(list)
+                binding.rvPost.adapter = adapter
+
+            }
+
+            override fun onFailure(call: Call<ArrayList<PostResponse>>, t: Throwable) {
+            }
+        })
 
         binding.ruanganggotaButton.setOnClickListener {
             val intent = Intent(this, RuangAnggota::class.java)
