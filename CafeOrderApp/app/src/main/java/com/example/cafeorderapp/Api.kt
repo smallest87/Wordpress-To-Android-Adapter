92ed94ac8249c9bf4d036ce6b95dd36df02d9112
Julian Sukrisna <smallest87@gmail.com>
2022-06-16 10:23:50 +0700

    Update Api.kt

--- CafeOrderApp/app/src/main/java/com/example/cafeorderapp/Api.kt
+++ CafeOrderApp/app/src/main/java/com/example/cafeorderapp/Api.kt
@@ -4,11 +4,21 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface Api {
-    @GET("posts?per_page=5")
+    @GET("posts?categories=21&per_page=5")
     fun getPosts(): Call<ArrayList<JSONResponse>>
 
     @GET("posts?per_page=3")
     fun getPostsTwo(): Call<ArrayList<JSONResponse>>
+
+//    https://javasatu.com/wp-json/wp/v2/posts?categories=15 > Olahraga
+//    23 > POLRI
+//    6 > Desa Kita
+//    19 > Pemerintahan
+//    20 > Pendidikan
+//    3056 > Organisasi
+//    5 > Covid-19
+//    21 > Peristiwa
+
 
 //    @GET("posts")
 //    fun getPosts(@Query("per_page") perPage : Int): Call<ArrayList<PostResponse>>
