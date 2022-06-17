0963d55a37b121d9caea32930fab9f5484229c55
Julian Sukrisna <smallest87@gmail.com>
2022-06-18 00:58:47 +0700

    Update Beranda.kt

--- CafeOrderApp/app/src/main/java/com/example/cafeorderapp/Beranda.kt
+++ CafeOrderApp/app/src/main/java/com/example/cafeorderapp/Beranda.kt
@@ -1,14 +1,11 @@
 package com.example.cafeorderapp
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cafeorderapp.databinding.LayarBerandaBinding
 import com.example.cafeorderapp.retrofit.RetrofitClient
 import com.example.cafeorderapp.retrofit.kumpulanDataJSONBeritaTerbaru
-import com.example.cafeorderapp.retrofit.kumpulanDataJSONPendidikan
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,21 +20,17 @@ class Beranda : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("TESD","Masuk ke sesi Debugging.")
         binding = LayarBerandaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.layout1RvTerbaru.setHasFixedSize(true)
         binding.layout1RvTerbaru.layoutManager = LinearLayoutManager(this)
-            Toast.makeText(this, "Tes Klik RV", Toast.LENGTH_SHORT).show()
 
         binding.layout1RvPendidikan.setHasFixedSize(true)
         binding.layout1RvPendidikan.layoutManager = LinearLayoutManager(this)
 
         binding.layout1RvPeristiwa.setHasFixedSize(true)
         binding.layout1RvPeristiwa.layoutManager = LinearLayoutManager(this)
-
-
 
         RetrofitClient.instance.ambilBeritaTerbaru(5,1,"id,title,date",null).enqueue(
             object: Callback<ArrayList<kumpulanDataJSONBeritaTerbaru>>{
@@ -48,10 +41,7 @@ class Beranda : AppCompatActivity() {
                 ) {
     //                val responseCode = response.code().toString()
                     response.body()?.let { kumpulanDataBeritaTerbaru.addAll(it)}
-
                     binding.layout1RvTerbaru.adapter = AdapterBeritaTerbaru(kumpulanDataBeritaTerbaru)
-//                    val adapterdua = AdapterTemplate02(list)
-//                    binding.layout1RvPost2.adapter = adapterdua
                 }
 
                 override fun onFailure(call: Call<ArrayList<kumpulanDataJSONBeritaTerbaru>>, t: Throwable) {
@@ -69,7 +59,6 @@ class Beranda : AppCompatActivity() {
                 ) {
                     //                val responseCode = response.code().toString()
                     response.body()?.let { kumpulanDataPendidikan.addAll(it)}
-
                     binding.layout1RvPendidikan.adapter = AdapterTemplate02(kumpulanDataPendidikan)
                 }
 
@@ -88,7 +77,6 @@ class Beranda : AppCompatActivity() {
                 ) {
                     //                val responseCode = response.code().toString()
                     response.body()?.let { kumpulanDataPeristiwa.addAll(it)}
-
                     binding.layout1RvPeristiwa.adapter = AdapterBeritaTerbaru(kumpulanDataPeristiwa)
                 }
 
